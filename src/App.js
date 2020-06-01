ed0e35154b6ed9e5ddd90dcada79cdc9159ae3b9
Carlos Salazar <carlosiusalazar@gmail.com>
2020-06-01 22:21:46 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,37 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from './firebaseConfig';
-const db = firebase.firestore();
+import APITest from './Components/APITest';
+//const db = firebase.firestore();
 
 
+//var promptStory = prompt("Please enter your prompt", "Harry Potter");
 
 
-var promptStory = prompt("Please enter your prompt", "Harry Potter");
-
-
-
-function savetoDatabase(promptStory) {
-  db.collection("Entries").add({
-          text: promptStory,
-      })
-      .then(function () {
-          console.log("Document successfully written!");
-          //window.location = "/";
-          alert("Game successfully saved!")
-      })
-      .catch(function (error) {
-          console.error("Error writing document: ", error);
-      });
-}
+// function savetoDatabase(promptStory) {
+//   db.collection("Entries").add({
+//           text: promptStory,
+//       })
+//       .then(function () {
+//           console.log("Document successfully written!");
+//           //window.location = "/";
+//           alert("Game successfully saved!")
+//       })
+//       .catch(function (error) {
+//           console.error("Error writing document: ", error);
+//       });
+// }
 
   
 
-setTimeout(function(){ savetoDatabase(promptStory) }, 3000);
+//setTimeout(function(){ savetoDatabase(promptStory) }, 3000);
 
 
 
 function TestDB() {
   return (
+    <div>
+    <APITest />
     <form>
       <label>
         Name:
@@ -40,17 +40,21 @@ function TestDB() {
       </label>
       <input type="submit" value="Submit" />
     </form>
+    </div>
   )
 }
 
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <TestDB />
       </header>
--- src/Components/APITest.js
+++ src/Components/APITest.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function APITest() {
 
+  const deepai = require('deepai'); 
+  const [outputText, setOutputText] = useState("Temp");
+  
+  function fetchAIStory(){
+    
+    deepai.setApiKey('85b4427e-815e-4efd-99e2-de892b8f2d22');
+  
+    (async function() {
+        let resp = await deepai.callStandardApi("text-generator", {
+                text: "My cats are playing, running around and making lots of noise",
+        });
+        console.log(resp);
+        console.log(resp.output)
+        receiveStory(resp.output);
+    })()
+  }
+  
+  fetchAIStory();
+  
+  function receiveStory(story){
+    setOutputText(story)
+  }
+
+
+
   return (
     <div>
-      <h3> This is APITest Component </h3>
+      <p>{outputText} Hello world 2!</p>
     </div>
   );
 };
