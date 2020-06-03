8804c5d1e96429b9161a3b48ab24201731ef6111
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-06-03 14:31:28 +0900

    Story and promises hell solved?

--- src/Components/displaystory/DisplayStory.js
+++ src/Components/displaystory/DisplayStory.js
@@ -6,66 +6,94 @@ const db = firebase.firestore();
 
 function DisplayStory() {
 
-  useEffect(() => {
-    fetchEntriesIdsForStory("7AHzkX74jsjsjsjsjsjsjs");
-	}, [])
+
 
 	const [ textArr, setTextArr ] = useState([]);
 	const [ storyArr, setStoryArr ] = useState([]);
   let newArr2 = [];
 
-  async function fetchEntryText(id) {
-  	let output = [];
-    db.collection('Entries').where('id', '==', id).get()
-    .then(function(querySnapshot) {
-			querySnapshot.forEach(function(doc) {
-        output.push(doc.data().text);
-			})
-      console.log("output is", output)
-		})
-		return output;
-  };
+  useEffect(() => {
+    fetchEntriesIdsForStory("7AHzkX74jsjsjsjsjsjsjs");
+	}, textArr)
 
-  async function fetchEntriesIdsForStory(id) {
-		let output = [];
+  // function fetchEntryText(id) {
+  // 	let output = [];
+  //   db.collection('Entries').where('id', '==', id).get()
+  //   .then(function(querySnapshot) {
+	// 		querySnapshot.forEach(function(doc) {
+  //       output.push(doc.data().text);
+	// 		})
+  //   console.log("output is", output)
+  //   })
+	// 	return output;
+  // };
+
+  // function fetchEntriesIdsForStory(id) {
+	// 	let output = [];
+	// 	let text_array = [];
+  //   db.collection('StoryDatabase').where('id', '==', id).get()
+  //   .then(function(querySnapshot) {
+	// 		querySnapshot.forEach(function(doc) {
+  //       // doc.data() is never undefined for query doc snapshots
+  //       let outputPromise = doc.data().entries.map((id) => {
+  //         let output = [];
+  //         db.collection('Entries').where('id', '==', id).get()
+  //         .then(function(querySnapshot) {
+  //           querySnapshot.forEach(function(doc) {
+  //             output.push(doc.data().text);
+  //           })
+  //         console.log("output is", output)
+  //         })
+  //         return outpu
+  //       })
+  //       console.log("output outputPromise", outputPromise)
+  //       newArr2 = Promise.all(outputPromise)
+  //       console.log("output newArr2", newArr2)
+  //       setStoryArr(outputPromise)
+  //   })})
+  // };
+  
+
+  function fetchEntriesIdsForStory(id) {
+		// let output = [];
 		let text_array = [];
     db.collection('StoryDatabase').where('id', '==', id).get()
     .then(function(querySnapshot) {
+      let ids_array = [];
 			querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-				output = doc.data().entries.map((id) => fetchEntryText(id))
-				console.log("OUTPUTS for storyArr", output);
-        Promise.all(output).then((output)=>{
-          setStoryArr(output);
-          newArr2.push(output)
-          console.log("storyArr",storyArr)
+      // ids_array.push(doc.data().entries.forEach(entry=>entry))
+      ids_array.push(doc.data().entries)
+      })
+      console.log("ids_array", ids_array.length)
+      return ids_array[0];
+    }
+    )
+    .then(ids_array => {
+        ids_array.forEach((id) => {
+          db.collection('Entries').where('id', '==', id).get()
+          .then(function(querySnapshot2) {
+            console.log("l74");
+		      	querySnapshot2.forEach(function(doc) {
+              console.log("doc.data().text", doc.data().text)
+              text_array.push(doc.data().text);
+              setStoryArr(storyArr => storyArr.concat(doc.data().text))
+              
+            })
+          })
         })
-        console.log("storyArr is",storyArr)
-        console.log("newArr2 is", newArr2)    
-    })})
-	};
-// console.log("storyArr",storyArr)
+    })
 
-  // fetchEntry("852ae944-dc26-4c9b-8b88-eecd9427d020");
-  // fetchStory();
-// let newArr3 = [];
-//   setTimeout(function(){  Promise.all(newArr).then(values => {
-//     console.log("values",values)
-//     newArr3.push(values)
-//   }) }, 3000);
-
-
+  };
+	 
+//  setTimeout(function(){   }, 500);
 	return (
 		<div className="DisplayStory">
-			Here is a story from Display Story:
-      <p>{newArr2}</p>
-			{/* <p>{fetchEntriesIdsForStory("7AHzkX74jsjsjsjsjsjsjs")}</p> */}
-      {/* {fetchEntriesIdsForStory("7AHzkX74jsjsjsjsjsjsjs").map((item) => {
-				return <p key={uuidv4()}>{item}</p>;
-			})} */}
+      Here is a story from Display Story:
+      {console.log("storyArr",storyArr)}
+      {<p>{storyArr}</p>}
 			<p>Test 2</p>
 		</div>
-	);
+  );
 }
 
 export default DisplayStory;
