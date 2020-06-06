45ffb1e1d948ed451d8bc12c33f4312b8ab263a6
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-06-06 15:39:50 +0900

    Added filter for genre

--- src/Components/storylist/StoryList.js
+++ src/Components/storylist/StoryList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './StoryList.styles.scss';
 import StoryPreview from '../storypreview/StoryPreview'
 import firebase from '../../firebaseConfig';
@@ -7,19 +7,51 @@ const db = firebase.firestore();
 
 function StoryList() {
   const [stories, setStories] = useState([]);
+  const [genre, setGenre] = useState("");
+  const storyGenre = useRef("");
 
   useEffect(() => {
-    retrieveAllStories();
-  }, [])
+    console.log('storyGenre in USE EFFECT', storyGenre.current.value);
+    
+    retrieveAllStories(storyGenre.current.value);
+  }, [genre])
 
-  const retrieveAllStories = async () => {
-    const data = await db.collection('StoryDatabase').orderBy('dateCreated').get();
-    setStories(stories => stories.concat(data.docs.map((doc) => doc.data())));
+  const selectGenre = () => {
+    setGenre(storyGenre.current.value)
+  }
+
+  const retrieveAllStories = async (genre) => {
+    // console.log('genre FOR STORY LIST', genre);
+
+    if (genre === "All") {
+      const data = await db.collection('StoryDatabase').orderBy('dateCreated').get();
+      setStories(stories => stories.concat(data.docs.map((doc) => doc.data())));
+    } else {
+      const data = await db.collection('StoryDatabase').where('genre', "==", genre).get();
+      setStories([]);
+      setStories(stories => stories.concat(data.docs.map((doc) => doc.data())));
+      console.log('data genre FOR STORY LIST ', data);
+    }
+
   };
-  
-
   return (
     <div className="DisplayStory">
+      <div className="form-group">
+        <label htmlFor="select-deadline">Filter Story Genre</label>
+        <select select="true" className = "form-control" id = "exampleFormControlSelect1" onChange={selectGenre} ref={storyGenre}>
+        <option>All</option>  
+        <option>Crime</option>
+        <option>Fan fiction</option>
+        <option>Fantasy</option>
+        <option>Historical</option>
+        <option>Horror</option>
+        <option>Humor</option>
+        <option>Romance</option>
+        <option>Sci-fi</option>
+        <option>Thriller</option>
+        <option>Other</option>
+        </select>
+      </div>
       <div className="row">
       {stories.map((story) => {         
         return (
