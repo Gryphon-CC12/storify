7715d033746abdfd165d29b2092195fd0f749d02
Carlos Salazar <carlosiusalazar@gmail.com>
2020-06-17 10:59:19 +0900

    Fixed Filters, Added About-Disclaimer, Added Favicon

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -7,12 +7,18 @@ function About() {
       <h1 style={{ color: "grey" }}> About Storify </h1>
       <br />
       <p>
-        Storify is a collaborative story-writing platform between human and
-        machine.{" "}
+        Storify is a collaborative story-writing platform between humans and
+        Artificial Intelligence.{" "}
       </p>
       <p>
         Join today to create your own story, invite friends to continue that
         story and watch robot take it to exciting unknown paths.
+      </p>
+      <p><b>
+        DISCLAIMER: Storify uses <a target="_blank" href="https://openai.com/blog/better-language-models/">Open AI GPT-2</a>, a large-scale unsupervised language model which generates coherent paragraphs of text and achieves state-of-the-art performance on many language modeling benchmarks. Given the unsupervised nature of GPT-2 it's impossible to predict the contents of the AI generated text. For this reason it's possible that swear words, sexual topics, and other NSFW text is generated and displayed within the story. Please keep this in mind whilst you use Storify.
+      </b></p>
+      <p>
+        Happy writing!.
       </p>
     </div>
   );
