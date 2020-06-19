ad7aec02a6c0b9205181068535e4271d72b90bb0
Carlos Salazar <carlosiusalazar@gmail.com>
2020-06-19 17:04:26 +0900

    Removed Text area on click, updated entries left, fixed user crashing bugs

--- src/Components/createstory/CreateStory.js
+++ src/Components/createstory/CreateStory.js
@@ -64,6 +64,7 @@ function CreateStory(props) {
         imageUrl: imageAsUrl,
         genre: storyGenre.current.value,
         timeLimit: deadline.current.value,
+        lastAuthor: user.email,
         isCompleted: Number(maxEntries.current.value) - 1 == 0
       })
       .then(function () {
@@ -93,6 +94,7 @@ function CreateStory(props) {
         imageUrl: imageAsUrl,
         genre: storyGenre.current.value,
         timeLimit: deadline.current.value,
+        lastAuthor: user.email,
         isCompleted: Number(maxEntries.current.value) - 1 == 0
       })
       .then(function () {
