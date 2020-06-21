10f927cd9c2351e1e6917e39e2ea88ff64cde7ae
pollyj <55009539+pollyj@users.noreply.github.com>
2020-06-21 21:59:46 +0900

    style about page

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import rightChevron from '../../assets/rightChevron.svg'
 import "./About.styles.scss";
 
 function About() {
@@ -6,65 +8,76 @@ function About() {
     <div id="about" className="container">
       <div className="row">
         <div className="col-12">
-          <h1 style={{ color: "grey" }}> About <span className="styled">Storify</span> </h1>
-            <br />
+          <div className="header">
+            <h1> About <span className="styled">Storify</span> </h1>
+          </div>
+  
+          <div className="box">
+          <h2>What is Storify?</h2>
             <p>
-              Storify is a collaborative story-writing platform between humans and
-              Artificial Intelligence.{" "}
+            Storify is a collaborative story-writing platform between humans and
+            artificial intelligence.{" "}
             </p>
             <p>
-              Join today to create your own story, invite friends to continue that
-              story, and watch the Storify Bot take your adventure to exciting, unknown places.
+              Create your own Story that others can participate in by clicking "Create Story", or browse the list of already submitted Stories for one to join. If you create your own Story, you can choose to have Robo-Writer participate too. Watch as the AI takes your adventures to exciting, unknown places!
             </p>
-            <br></br>
-            <b>How does Storify work?</b>
-            <p>Anyone can join and collaborate in Storify! Please take a moment to read the rules below to have the best possible experience.</p>
-          <ul>
-            <li>
-              🖋 Create a Story. The story creator decides key elements of the story.
-            </li> 
-            <li>  
-              🖋 Set a title of the story. 
-            </li> 
-            <li>  
-              🖋 Add an image that represents the story (a default image will be used if none is uploaded). 
-            </li> 
-            <li>  
-              🖋 Enter your story prompt. This will be the beginning of the story to which your collaborators and Robo-Writer will contribute.
-            </li>      
-            <li>
-              🖋 Robo-Writer. You can choose to add Robo-Writer to contribute in your story when it is it's turn to write. 
-            </li>
-            <li>
-              🖋 Private Stories. A private story is hidden from the main story list page. You can share the link to people you'd like to collaborate with you.  Keep in mind that anyone with the link can read and may join as collaborators if there are available slots. A private story cannot be made into a public story after the story prompt is created.
-            </li>
-            <li>
-              🖋 Set maximum number of Entries and Human Collaborators. Number of Human collaborators should not exceed the number of entries so each colaborator could add at least one entry.
-            </li>
+            </div>
+          
+            <div className="box">
+          <h2>How does Storify work?</h2>
+            <p>Anyone can join and collaborate in Storify!<br />Please take a moment to read the information below to have the best possible experience.</p>
+            <h3>Creating Your Own Stories</h3>
+            <p className="create-link">
+            <Link to="/about"><img src={rightChevron} alt="right icon" /> Create Story page</Link>
+            </p>
+              <ul>
+                <li>  
+                  Title:<br/>The title for your Story. 
+                </li> 
+                <li>  
+                  Text:<br/>This will be the beginning of the Story to which any collaborators will contribute.
+                </li> 
+                <li>  
+                  Image:<br/>You can choose to add  an image that helps set the scene for the Story. (A default image will be used if none is uploaded). 
+                </li>      
+                <li>
+                  Number of Human Collaborators:<br/>How many human participants the Story will have. Cannot be more than the number of entries set. Selecting 'one' will mean you are the only person able to participate.
+                </li>
+                <li>
+                  Use Robo-Writer:<br/>You can choose to add Robo-Writer to contribute in your Story when it is its turn to write. 
+                </li>
+                <li>
+                  Private Story:<br/>A private Story is hidden from the main list. You can share the link to people you'd like to collaborate with you. Keep in mind that anyone with the link can read and may join as collaborators if there are available slots. A private Story cannot be made into a public Story after the Story prompt is created.
+                </li>
+                <li>
+                  Number of Entries:<br/>How many entries you'd like the Story to have before it's finished.
+                </li>
+              </ul>
 
-          </ul>
-          <p>Collaborate in a Story.</p> 
-          <ul>
-            <li>
-              🖋 You can join and collaborate any story provided that there are empty collaborator slots.  Click on 'Join as Collaborator' button to add yourself to the story. You will be added to the list and when your turn is up you'll be notified along with the time limit you have to submit your entry. 
-            </li>
-            <li>
-              🖋 Story Flow. Keep in mind once you submit an entry it cannot be changed. This is done so the flow of the story is not altered particularly after other users have continued where you left at.
-            </li>
-            <li>
-              🖋 Entry Deadline. Make sure you’re not missing your turns. You will be skipped over if you don’t add your entry by the end of the designated time limit (e.g. if it’s 5 minutes, you have to reply within five minutes or the next player in the queue will become “in turn.”. You can see the deadline time near the text input area.
-            </li>
-            <li>
-              🖋 Pass turns. You can pass your turn to the next collaborator in line if you do not feel like submitting an entry on your turn.
-            </li>
-          </ul>
-          <ul>
-            <li>
-            <b>
-              🖋 DISCLAIMER: Storify uses <a target="_blank" rel="noopener noreferrer" href="https://openai.com/blog/better-language-models/">Open AI GPT-2</a>, a large-scale, unsupervised language model to generate coherent paragraphs of text. Given the unsupervised nature of Open AI GPT-2, it's impossible to predict the contents of the AI-generated text. For this reason, it's possible that swear words, sexual topics, or other NSFW elements may be included in the story. Please keep this in mind while using Storify.
-            </b>
-            </li>
-           </ul> 
+          <h3>Collaborating in Others' Stories</h3> 
+            <ul>
+              <li>
+                Joining:<br/>You can join and collaborate any Story provided there are still empty collaborator slots. Stories that have empty slots will have a 'Join the Story' button at the bottom of the Story's page. Once you join, you will be added to the collaborator list and when it becomes your turn, you'll receive an email notification stating when you should submit your entry by. (The deadline is decided by the Story creator.) 
+              </li>
+              <li>
+                Story Flow:<br/>Keep in mind that once you submit an entry, it cannot be changed. This is done so the flow of the story is not altered, particularly after other users have continued on from where your original entry left off at.
+              </li>
+              <li>
+                Entry Deadline:<br/>Make sure you’re not missing your turns. You will be skipped over if you don’t add your entry by the end of the designated time limit (e.g. if it’s 5 minutes, you have to add your entry within five minutes or the next player in the queue will become “in turn”. You can see the deadline next to the text input area where you add your entries.
+              </li>
+              <li>
+                Passing Turns:<br/>You can pass your turn to the next collaborator in line if you do not feel like submitting an entry during your turn. If you don't manage to submit an entry by your deadline, you will also forfeit your turn.
+              </li>
+            </ul>
+            </div>
+          
+          <div className="disclaimer-box">
+          <h2 className="disclaimer-heading">Disclaimer:</h2>
+          <p className="disclaimer">
+            Storify uses <a target="_blank" rel="noopener noreferrer" href="https://openai.com/blog/better-language-models/">Open AI GPT-2</a>, a large-scale, unsupervised language model to generate coherent paragraphs of text. Given the unsupervised nature of Open AI GPT-2, it's impossible to predict the contents of the AI-generated text. For this reason, it's possible that swear words, sexual topics, or other NSFW elements may be included in the story. Please keep this in mind while using Storify.
+          </p>
+          </div>
+
             <p className="styled bigger">
               Happy writing!
             </p>
